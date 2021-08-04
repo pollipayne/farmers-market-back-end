04752c8eacbf294805003216b8487ab90a204858
Brittney Payne <ohoptional@gmail.com>
2021-08-04 15:48:14 -0700

    added route to add vendor to specific market

--- src/services/VendorService.ts
+++ src/services/VendorService.ts
@@ -1,15 +1,16 @@
 import { getConnection, getRepository } from 'typeorm';
 import { Vendor } from '../entities/Vendor';
 
-
+import { MarketRepository } from '../repository/MarketRepository';
 import { VendorRepository } from '../repository/VendorRepository';
 
 export class VendorService {
   private vendorRepository: VendorRepository;
+  private marketRepository: MarketRepository
 
   constructor() {
     this.vendorRepository = getConnection('user').getCustomRepository(VendorRepository)
-
+    this.marketRepository = getConnection('user').getCustomRepository(MarketRepository)
   }
 
   public index = async () => {
@@ -18,8 +19,21 @@ export class VendorService {
     })
     return vendors;
   }
-  public create = async (vendors: Vendor) => {
-    const newVendor = await this.vendorRepository.save(vendors);
+  public create = async (vendors: Vendor, marketId: number) => {
+    const newVendor = vendors
+    const associatedMarket = await this.marketRepository.findOne(marketId)
+    // console.log(associatedMarket)
+    if (associatedMarket && newVendor.markets) {
+      newVendor.markets?.push(associatedMarket)
+
+    } else if (associatedMarket) {
+      newVendor.markets = []
+      newVendor.markets.push(associatedMarket)
+    } else {
+      throw new Error("Couldn't find associated market")
+    }
+    await this.vendorRepository.save(vendors);
+
     return newVendor;
   }
   public update = async (vendor: Vendor, id: number) => {
