0f03cff7a36c70b33412ad6583e6b1810020e639
Brittney Payne <ohoptional@gmail.com>
2021-08-13 11:22:41 -0700

    Heroku

--- src/Server.ts
+++ src/Server.ts
@@ -39,7 +39,7 @@ export class Server {
       type: "postgres",
       host: process.env.DB_HOSTNAME,
       port: 5432,
-      ssl: true,
+      ssl: { rejectUnauthorized: false },
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
